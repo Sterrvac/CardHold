744d9db647dc5ed3e35caf2e15e405cf620b1616
Sterrvac <Slavok.slavok2000@yandex.ru>
2023-11-07 21:19:57 +0300

    Create Project

---
+++ CardHold/CardHoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardHoldApp.swift
+//  CardHold
+//
+//  Created by macbook on 07.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardHoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

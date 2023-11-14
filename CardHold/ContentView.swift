87d2fa60393d959f2bd69a1cd56521e12d24bc79
Sterrvac <Slavok.slavok2000@yandex.ru>
2023-11-14 23:11:07 +0300

    add element Main View and addeting Git Ignore

--- CardHold/ContentView.swift
+++ CardHold/ContentView.swift
@@ -1,21 +1,26 @@
-//
-//  ContentView.swift
-//  CardHold
-//
-//  Created by macbook on 07.11.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var title = "Welcome"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(spacing: 200) {
+            Text("Карты")
+                .padding(30)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.black)
+                .background(Color(.sRGB, red: 230/255, green: 210/255, blue: 0/255, opacity: 1.0))
+                .font(.custom("Sun", size: 36, relativeTo: .body))
+                .fontWeight(.ultraLight)
+                .cornerRadius(10)
+                .tracking(10)
+            Text("Добро пожаловать")
+                .font(.title)
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarRole(.editor)
+            Spacer()
+        }.padding(10)
     }
 }
 
